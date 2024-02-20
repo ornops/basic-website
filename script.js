8234a9bcd20f7a308926e6b88336a837eb6aae1f
ornops <joshuaornopia@gmail.com>
2024-02-20 14:40:00 +0800

    Updated responsiveness of Header and Navbar

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
         slideMargin:0,
         loop:true
     });
+
+    $('.main-menu > ul:nth-of-type(2)').hide()
+
+    $('.toggler').click(function(){
+        $('.main-menu > ul:nth-of-type(2)').toggle();
+        $('.drop').css({'display':'block','position':'relative'});
+    })
 });
